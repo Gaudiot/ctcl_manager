401211ae8f53ddc343cd1cf5fee090541e9c3269
gaudiot <victor.gaudiot@uber.com>
2025-11-07 11:55:21 -0300

    update read operations to new schema

--- lib/base/DAOs/interface.dao.dart
+++ lib/base/DAOs/interface.dao.dart
@@ -5,6 +5,7 @@ abstract interface class BaseDAO<T, E extends Exception> {
   IDatabaseClient get databaseClient;
 
   Future<Result<T, E>> create(T data);
+  Future<Result<List<T>, E>> getAll();
   Future<Result<T, E>> getById(String id);
   Future<Result<T, E>> updateById(String id, T data);
   Future<Result<void, E>> deleteById(String id);
--- lib/base/DAOs/models/student.dao_model.dart
+++ lib/base/DAOs/models/student.dao_model.dart
@@ -3,9 +3,10 @@ final class StudentDAOModel {
   final String firstName;
   final String lastName;
   final String phone;
-  final String emailAddress;
-  final DateTime birthday;
-  final String instagram;
+  final String? emailAddress;
+  final DateTime? birthday;
+  final String? instagram;
+  final String? classId;
   final DateTime createdAt;
   final DateTime updatedAt;
 
@@ -14,10 +15,28 @@ final class StudentDAOModel {
     required this.firstName,
     required this.lastName,
     required this.phone,
-    required this.emailAddress,
-    required this.birthday,
-    required this.instagram,
     required this.createdAt,
     required this.updatedAt,
+    this.emailAddress,
+    this.birthday,
+    this.instagram,
+    this.classId,
   });
+
+  factory StudentDAOModel.fromJson(Map<String, dynamic> json) {
+    return StudentDAOModel(
+      id: json["id"] as String,
+      firstName: json["first_name"] as String,
+      lastName: json["last_name"] as String,
+      phone: json["phone"] as String,
+      emailAddress: json["email_address"] as String?,
+      birthday: json.containsKey("birthday")
+          ? DateTime.parse(json["birthday"] as String)
+          : null,
+      instagram: json["instagram"] as String?,
+      classId: json["class_id"] as String?,
+      createdAt: DateTime.parse(json["created_at"] as String),
+      updatedAt: DateTime.parse(json["updated_at"] as String),
+    );
+  }
 }
